e0370a75198db86ecf5a607b6604765a8962d60c
Matteo Del Fabbro <6kctmmjpvb@privaterelay.appleid.com>
2026-07-26 12:56:44 +0200

    Completa intro e sillabe di Dall'aurora

--- public/js/app.js
+++ public/js/app.js
@@ -111,7 +111,7 @@ function legacyBookNumber(song){
 
 
 async function loadSongs() {
-  const songDataVersion='20260726-dallaurora-60';
+  const songDataVersion='20260726-dallaurora-61';
   const response = await fetch('./data/songs-index.json');
   if (!response.ok) throw new Error('Impossibile caricare l’indice dei canti.');
   const songIndex = await response.json();
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'canzoniere-dallaurora-60';
+const CACHE_NAME = 'canzoniere-dallaurora-61';
 const APP_ASSETS = [
   "./",
   "./index.html",

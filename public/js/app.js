8c19c942edf326ea8c028ef27261ae46ff5f6090
Matteo Del Fabbro <6kctmmjpvb@privaterelay.appleid.com>
2026-07-26 13:54:04 +0200

    Fa scorrere i comandi su tutti gli smartphone

--- public/js/app.js
+++ public/js/app.js
@@ -111,7 +111,7 @@ function legacyBookNumber(song){
 
 
 async function loadSongs() {
-  const songDataVersion='20260726-iphone-scroll-toolbar-1';
+  const songDataVersion='20260726-phone-scroll-toolbar-2';
   const response = await fetch('./data/songs-index.json');
   if (!response.ok) throw new Error('Impossibile caricare l’indice dei canti.');
   const songIndex = await response.json();
